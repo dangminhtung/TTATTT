5b1aa36cd2c023df82f44adb6a67b38d7e0fd7bb
dangminhtung <minhtungd2402@gmail.com>
2022-11-12 19:53:52 +0700

    ttattt

--- 36.cpp
+++ 36.cpp
@@ -1,81 +1,59 @@
-#include <stdio.h>
+#include <iostream>
 #include <math.h>
-#include <string.h>
+#include <cstring>
+// chuỗi p là gốc còn biến xét theo chuỗi T
+using namespace std;
 int main()
 {
+
     char t[1000], p[1000];
-    int l[127];
-    printf("\nNhap T: ");
-    fgets(t, sizeof(t), stdin);
-    printf("\nNhap P: ");
-    fgets(p, sizeof(p), stdin);
-    printf("%d", strlen(p));
-    int lap = 0;
-    for (int i = 0; i < 127; i++)
+    int l[1000];
+    cout << "nhap chuoi T: ";
+    cin >> t;
+    cout << "nhap chuoi P: ";
+    cin >> p;
+    for (int i = 0; i <= 127; i++)
     {
         l[i] = -1;
     }
-    for (int i = 0; i < (int)strlen(p) - 1; i++)
+    // các giá trị assci là chỉ số còn giá trị mảng l tăng dần
+    for (int i = 0; i < (int)strlen(p); i++)
     {
         l[(int)p[i]] = i;
     }
-    int cong_them, i, j, d = 0;
-    for (i = 0; i < (int)strlen(p) - 1; i++)
-    {
-        printf("%c ", p[i]);
-        printf("%d \n", l[(int)p[i]]);
-    }
-    i = strlen(p) - 2;
-    j = strlen(p) - 2;
-    d = 0;
-    printf("i=%d j=%d\n", i, j);
-
-    while (i < strlen(t) - 1)
+    int d = 0;
+    int i = strlen(p) - 1,
+        j = strlen(p) - 1;
+    while (i < strlen(t))
     {
         d = 0;
-        while (d != strlen(p) - 1)
+        while (d < strlen(p))
         {
-            if (t[i] == p[j])
+            if (p[j] == t[i])
             {
-                // printf("t[%d] = p[%d] = %c\n", i, j, t[i]);
                 i--;
                 j--;
                 d++;
-                lap++;
-                //         printf("i=%d j=%d\n",i,j);
             }
             else
             {
+                // so sánh giá trị i đc xets với các phần tử của p thông qua mảng l
                 int min;
-                printf("ket qua %d ", i);
-                if (j > 1 + l[(int)t[i]])
+                if (j > l[(int)t[i]])
                 {
                     min = 1 + l[(int)t[i]];
                 }
-                else
-                {
-                    min = j;
-                }
-                printf("Chu cai t la %c\n", t[i]);
-                printf("i=%d j=%d min=%d\n", i, j, min);
-                printf("1+ l=%d\n", l[(int)t[i]] + 1);
-                if (min == 0)
-                    i = i + strlen(p) - min - 1;
-                if (min != 0)
-                    i = i + strlen(p) - min - 1;
-                j = strlen(p) - 1 - 1;
-                printf("=> i=%d j=%d\n", i, j);
-                lap++;
+                // tịnh tiến giá trí i của mảng T để so sánh tiếp sao cho giá trị i trùng với giá trị j, mặc định giá trị j bằng chiều dài p, chỉ thay đổi i
+                i = i + strlen(p) - min;
                 break;
             }
         }
-        if (d == strlen(p) - 1)
+
+        if (d == strlen(p))
         {
-            printf("P co trong T, bat dau tai vi tri %d\n", i + 2);
-            printf("\nLap: %d", lap);
+            cout << "chuoi p co trong t bat dau tu vi tri: " << i + 2 << endl;
             return 0;
         }
     }
-    printf("P khong co trong T\n");
-    printf("\nLap: %d", lap);
+    cout << "khong xuat hien trong chuoi T";
 }
--- test1.cpp
+++ test1.cpp
@@ -1,13 +1,59 @@
 #include <iostream>
 #include <math.h>
 #include <cstring>
-
+// chuỗi p là gốc còn biến xét theo chuỗi T
 using namespace std;
 int main()
 {
 
-    char c[10];
-    cin >> c;
-    // fgets(c, sizeof(c) + 1, stdin);
-    cout << strlen(c);
+    char t[1000], p[1000];
+    int l[1000];
+    cout << "nhap chuoi T: ";
+    cin >> t;
+    cout << "nhap chuoi P: ";
+    cin >> p;
+    for (int i = 0; i <= 127; i++)
+    {
+        l[i] = -1;
+    }
+    // các giá trị assci là chỉ số còn giá trị mảng l tăng dần
+    for (int i = 0; i < (int)strlen(p); i++)
+    {
+        l[(int)p[i]] = i;
+    }
+    int d = 0;
+    int i = strlen(p) - 1,
+        j = strlen(p) - 1;
+    while (i < strlen(t))
+    {
+        d = 0;
+        while (d < strlen(p))
+        {
+            if (p[j] == t[i])
+            {
+                i--;
+                j--;
+                d++;
+            }
+            else
+            {
+                // so sánh giá trị i đc xets với các phần tử của p thông qua mảng l
+                int min;
+                if (j > l[(int)t[i]])
+                {
+                    min = 1 + l[(int)t[i]];
+                }
+                // tịnh tiến giá trí i của mảng T để so sánh tiếp sao cho giá trị i trùng với giá trị j, mặc định giá trị j bằng chiều dài p, chỉ thay đổi i
+                i = i + strlen(p) - min;
+                break;
+            }
+        }
+
+        if (d == strlen(p))
+        {
+            cout << "chuoi p co trong t bat dau tu vi tri: " << i + 2 << endl;
+            return 0;
+        }
+    }
+    cout << "khong xuat hien trong chuoi T";
 }
